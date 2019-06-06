1195cd3d73cc2abf686da3f5051f22931993beed
HenryGuo <guohongyi@sjtu.edu.cn>
2019-06-06 10:28:47 +0800

    add rule-based

---
+++ baselines/stochastic/rule.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class Rule(object):
+    """
+    A rule-based model
+    """
+
+    def __init__(self, env):
+        self.act_size = env.n
+        self.n_actions = env.n_actions
+
+    def step(self, obs):
+        return np.random.choice(self.act_size-1, self.n_actions), 0
+
+    def train(self, obs, d_rewards, d_actions, d_values):
+        pass
+
+    def output(self, info):
+        pass
